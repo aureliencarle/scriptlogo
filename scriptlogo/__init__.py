305e94b1b1de31c4712d0ad001401d29cb56f73a
Aurelien CARLE <aurelien.carle@gmail.com>
2022-02-10 11:49:12 +0100

    fill __init__.py

---
+++ scriptlogo/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+ 
+"""
+    Ce module propose les outils les plus courrant que j'utilise,
+    et aussi pour retrouver des syntaxes peu utilisée que je n'ai pas en mémoire,
+    et qui nécessiteraient des recherches.
+    Les imports réalisé ici facilite les imports dans les scripts:
+    Au lieu de:
+        from scriptlogo.shell_logo import Logo
+    faire:
+        from scriptlogo import Logo
+"""
+
+name = "scriptlogo"
+
+from scriptlogo.shell_logo import Logo
